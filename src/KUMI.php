8167a26a720b00c15e20dfe2d39f0816dc5d2959
laszlobravik <braviklacy@msn.com>
2022-03-22 10:28:14 +0100

    2022_03_22

--- src/KUMI.php
+++ src/KUMI.php
@@ -229,6 +229,8 @@ class KUMI
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		$response = curl_exec($ch);
 		curl_close($ch);
 		$return_data= json_decode($response);
